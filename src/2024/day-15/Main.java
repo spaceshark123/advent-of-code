b28b80d40df51409dd0c5084d8257ec41834b982
spaceshark123 <amogh.patnaik@icloud.com>
2024-12-16 00:57:18 -0600

    solve day 15

--- src/2024/day-15/Main.java
+++ src/2024/day-15/Main.java
@@ -3,13 +3,287 @@ import java.io.*;
 import java.lang.*;
 
 class Main {
+	static final int[] dxs = new int[] { -1, 1, 0, 0 };
+	static final int[] dys = new int[] { 0, 0, -1, 1 };
+	static HashMap<Character, Integer> dirMap = new HashMap<>(4);
+
+	static int HEIGHT;
+	static int WIDTH;
+
 	public static void main(String[] args) throws IOException {
 		// read input from file
-		Kattio io = new Kattio("day 15", System.out);
+		Kattio io = new Kattio("day15", System.out);
 
-		
+		// directions
+		dirMap.put('<', 0);
+		dirMap.put('>', 1);
+		dirMap.put('^', 2);
+		dirMap.put('v', 3);
+
+		// input
+		String[] lines = io.getAll().split("\n");
+		int i;
+		for (i = 0; i < lines.length; i++) {
+			if (lines[i].equals("") && i++ < lines.length) {
+				break;
+			}
+			HEIGHT++;
+			WIDTH = lines[i].length();
+		}
+		char[][] grid = new char[HEIGHT][WIDTH];
+		for (int j = 0; j < HEIGHT; j++) {
+			grid[j] = lines[j].toCharArray();
+		}
+		String movesStr = "";
+		String line;
+		while (i < lines.length && (line = lines[i++]) != null) {
+			//System.out.println("line: " + line);
+			movesStr += line;
+		}
+		char[] moves = movesStr.toCharArray();
+		char[][] gridCopy = new char[HEIGHT][WIDTH];
+		for (int j = 0; j < HEIGHT; j++) {
+			gridCopy[j] = grid[j].clone();
+		}
+
+		// part 1
+		long part1 = part1(grid, moves);
+		io.println(part1);
+
+		// part 2
+		long part2 = part2(gridCopy, moves);
+		io.println(part2);
 
 		io.close();
+	}
+
+	static long part1(char[][] grid, char[] moves) {
+		// find the starting position (@)
+		int x = 0;
+		int y = 0;
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				if (grid[i][j] == '@') {
+					x = j;
+					y = i;
+					break;
+				}
+			}
+		}
+		// simulate all moves
+		for (char move : moves) {
+			int dx = dxs[dirMap.get(move)];
+			int dy = dys[dirMap.get(move)];
+			int nx = x + dx;
+			int ny = y + dy;
+			if (nx < 0 || nx >= WIDTH || ny < 0 || ny >= HEIGHT || grid[ny][nx] == '#') {
+				continue;
+			}
+			// while the next cell is pushable
+			if (grid[ny][nx] == 'O') {
+				boolean push = true;
+				while (nx >= 0 && nx < WIDTH && ny >= 0 && ny < HEIGHT && grid[ny][nx] == 'O') {
+					int nnx = nx + dx;
+					int nny = ny + dy;
+					if (nnx < 0 || nnx >= WIDTH || nny < 0 || nny >= HEIGHT || grid[nny][nnx] == '#') {
+						push = false; // if the block can't be pushed, don't push it
+						break;
+					}
+					if (grid[nny][nnx] == '.') {
+						nx = nnx;
+						ny = nny;
+						break; // if theres an empty space, the block can be pushed
+					}
+					nx = nnx;
+					ny = nny;
+				}
+				// nx, ny is the last position of everything that is moving (player and blocks)
+				// push all objects from the current position to the next position
+				if (push) {
+					// start from nnx, nny and move back to x, y (shifting everything to the next position)
+					while (nx != x || ny != y) {
+						int nnx = nx - dx;
+						int nny = ny - dy;
+						grid[ny][nx] = grid[nny][nnx];
+						nx = nnx;
+						ny = nny;
+					}
+					// move the player
+					grid[ny][nx] = '.';
+					x = nx + dx;
+					y = ny + dy;
+				}
+			} else {
+				// if the next cell is empty, move the player
+				if (grid[ny][nx] == '.') {
+					grid[y][x] = '.';
+					grid[ny][nx] = '@';
+					x = nx;
+					y = ny;
+				}
+			}
+		}
+		return sumGPS(grid);
+	}
+
+	static long part2(char[][] grid, char[] moves) {
+		// scale up grid
+		char[][] newGrid = new char[HEIGHT][WIDTH * 2];
+		int x = 0;
+		int y = 0;
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				char c = grid[i][j];
+				if (c == '#') {
+					newGrid[i][j * 2] = '#';
+					newGrid[i][j * 2 + 1] = '#';
+				} else if (c == 'O') {
+					newGrid[i][j * 2] = '[';
+					newGrid[i][j * 2 + 1] = ']';
+				} else if (c == '.') {
+					newGrid[i][j * 2] = '.';
+					newGrid[i][j * 2 + 1] = '.';
+				} else if (c == '@') {
+					newGrid[i][j * 2] = '@';
+					x = j * 2;
+					y = i;
+					newGrid[i][j * 2 + 1] = '.';
+				}
+			}
+		}
+		// simulate all moves
+		for (char move : moves) {
+			int dx = dxs[dirMap.get(move)];
+			int dy = dys[dirMap.get(move)];
+			int nx = x + dx;
+			int ny = y + dy;
+			if (nx < 0 || nx >= WIDTH * 2 || ny < 0 || ny >= HEIGHT || newGrid[ny][nx] == '#') {
+				continue; // if the next cell is a wall, don't move
+			}
+			// if theres some pushing to be done
+			if (newGrid[ny][nx] == '[' || newGrid[ny][nx] == ']') {
+				boolean push = true;
+				// since a box is 2 wide ([]), it can push multiple boxes at once
+				HashSet<String> pushed = new HashSet<>(); // for finding all boxes to push
+				HashSet<String> toPush = new HashSet<>(); // boxes to push
+				if (newGrid[ny][nx] == '[') {
+					toPush.add(ny + "," + nx);
+					toPush.add(ny + "," + (nx + 1));
+					pushed.add(ny + "," + nx);
+					pushed.add(ny + "," + (nx + 1));
+				} else if (newGrid[ny][nx] == ']') {
+					toPush.add(ny + "," + nx);
+					toPush.add(ny + "," + (nx - 1));
+					pushed.add(ny + "," + nx);
+					pushed.add(ny + "," + (nx - 1));
+				}
+				while (!pushed.isEmpty()) {
+					// go in the direction of the push and check if the next cell is pushable
+					// extract coordinates from the set
+					String[] coord = pushed.iterator().next().split(",");
+					pushed.remove(coord[0] + "," + coord[1]);
+					int nnx = Integer.parseInt(coord[1]);
+					int nny = Integer.parseInt(coord[0]);
+					if (nnx < 0 || nnx >= WIDTH * 2 || nny < 0 || nny >= HEIGHT || newGrid[nny+dy][nnx+dx] == '#') {
+						//System.out.println("Push: false triggered at " + nnx + ", " + nny);
+						push = false; // if the block can't be pushed, don't push it
+						break;
+					}
+					if(newGrid[nny][nnx] == '[') {
+						if (!toPush.contains(nny + "," + (nnx + 1))) {
+							toPush.add(nny + "," + (nnx + 1));
+							pushed.add(nny + "," + (nnx + 1));
+						}
+					} else if(newGrid[nny][nnx] == ']') {
+						if (!toPush.contains(nny + "," + (nnx - 1))) {
+							toPush.add(nny + "," + (nnx - 1));
+							pushed.add(nny + "," + (nnx - 1));
+						}
+					}
+					if (newGrid[nny+dy][nnx+dx] == '.') {
+						nx = nnx+dx;
+						ny = nny+dy;
+						continue; // if theres an empty space, the block can be pushed
+					}
+					if (newGrid[nny + dy][nnx + dx] == '[') {
+						if (!toPush.contains((nny + dy) + "," + (nnx + dx))) {
+							toPush.add((nny + dy) + "," + (nnx + dx));
+							pushed.add((nny + dy) + "," + (nnx + dx));
+						}
+					} else if (newGrid[nny + dy][nnx + dx] == ']') {
+						if (!toPush.contains((nny + dy) + "," + (nnx + dx))) {
+							toPush.add((nny + dy) + "," + (nnx + dx));
+							pushed.add((nny + dy) + "," + (nnx + dx));
+						}
+					}
+				}
+				// sort the boxes to push by dy, dx
+				if (push) {
+					ArrayList<String> sorted = new ArrayList<>(toPush);
+					final int innerDx = dx;
+					final int innerDy = dy;
+					Collections.sort(sorted, (a, b) -> {
+						String[] aCoord = a.split(",");
+						String[] bCoord = b.split(",");
+						int x1 = Integer.parseInt(aCoord[0]);
+						int y1 = Integer.parseInt(aCoord[1]);
+						int x2 = Integer.parseInt(bCoord[0]);
+						int y2 = Integer.parseInt(bCoord[1]);
+
+						// Sort logic based on dx, dy
+						if (innerDy == 0) { // Horizontal sorting
+							if (y1 == y2) {
+								return (x1 - x2); 
+							}
+							return (y1 - y2) * -innerDx; // Sort by x depending on direction of dx
+						} else if (innerDx == 0) { // Vertical sorting
+							if (x1 == x2) {
+								return (y1 - y2); // Sort by y depending on direction of dy
+							}
+							return (x1 - x2) * -innerDy; // Sort by y depending on direction of dy
+						}
+						return 0; // Fallback
+					});
+					// go through the boxes and shift them
+					for (String coord : sorted) {
+						String[] c = coord.split(",");
+						nx = Integer.parseInt(c[1]);
+						ny = Integer.parseInt(c[0]);
+						int nnx = nx + dx;
+						int nny = ny + dy;
+						newGrid[nny][nnx] = newGrid[ny][nx];
+						newGrid[ny][nx] = '.';
+					}
+					// move the player
+					newGrid[y][x] = '.';
+					newGrid[y + dy][x + dx] = '@';
+					x = x + dx;
+					y = y + dy;
+				}
+			} else {
+				// if the next cell is empty, move the player
+				if (newGrid[ny][nx] == '.') {
+					newGrid[y][x] = '.';
+					newGrid[ny][nx] = '@';
+					x = nx;
+					y = ny;
+				}
+			}
+		}
+		return sumGPS(newGrid);
+	}
+
+	// sum GPS coordinates of all blocks (100 * first coordinate + second coordinate)
+	static long sumGPS(char[][] grid) {
+		long sum = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (grid[i][j] == 'O' || grid[i][j] == '[') {
+					sum += 100 * i + j;
+				}
+			}
+		}
+		return sum;
 	}
 
 	static class Kattio extends PrintWriter {
