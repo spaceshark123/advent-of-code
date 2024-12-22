d23b67bf594dbcecd599e472d5f188aff4a2eaa1
spaceshark123 <amogh.patnaik@icloud.com>
2024-12-22 00:59:28 -0600

    update template Java file with useful utilities: directions, timeIt function, more input functions, inBounds function, ArrayWrapper class

--- src/template/Main.java
+++ src/template/Main.java
@@ -1,8 +1,14 @@
 import java.util.*;
+import java.util.stream.*;
 import java.io.*;
 import java.lang.*;
 
 class Main {
+	static int[] dx = { 0, 1, 0, -1 }; // down, right, up, left
+	static int[] dy = { 1, 0, -1, 0 };
+	static int[] dx8 = { 0, 1, 1, 1, 0, -1, -1, -1 }; // down, DR, right, UR, up, UL, left, DL
+	static int[] dy8 = { 1, 1, 0, -1, -1, -1, 0, 1 };
+
 	public static void main(String[] args) throws IOException {
 		// read input from file
 		Kattio io = new Kattio("__input__", System.out);
@@ -17,10 +23,12 @@ class Main {
 			super(new BufferedOutputStream(System.out));
 			r = new BufferedReader(new InputStreamReader(i));
 		}
+
 		public Kattio(InputStream i, OutputStream o) {
 			super(new BufferedOutputStream(o));
 			r = new BufferedReader(new InputStreamReader(i));
 		}
+
 		public Kattio(String problemName) throws FileNotFoundException {
 			super(new BufferedOutputStream(new FileOutputStream(problemName + ".out")));
 			try {
@@ -30,6 +38,7 @@ class Main {
 				throw e; // Re-throw the exception to notify the caller
 			}
 		}
+
 		public Kattio(String problemName, OutputStream o) throws FileNotFoundException {
 			super(new BufferedOutputStream(o));
 			try {
@@ -48,7 +57,7 @@ class Main {
 			try {
 				// Mark the current position in the stream
 				r.mark(1000); // 1000 is the buffer size for mark/reset
-				
+
 				// Try to read the next line
 				if (r.readLine() != null) {
 					// If a line is available, reset the reader to the marked position
@@ -66,7 +75,7 @@ class Main {
 			return Integer.parseInt(nextToken());
 		}
 
-		public double getDouble() { 
+		public double getDouble() {
 			return Double.parseDouble(nextToken());
 		}
 
@@ -84,15 +93,17 @@ class Main {
 		private String token;
 
 		private String peekToken() {
-			if (token == null) 
+			if (token == null)
 				try {
-				while (st == null || !st.hasMoreTokens()) {
-					line = r.readLine();
-					if (line == null) return null;
-					st = new StringTokenizer(line);
+					while (st == null || !st.hasMoreTokens()) {
+						line = r.readLine();
+						if (line == null)
+							return null;
+						st = new StringTokenizer(line);
+					}
+					token = st.nextToken();
+				} catch (IOException e) {
 				}
-				token = st.nextToken();
-				} catch (IOException e) { }
 			return token;
 		}
 
@@ -101,7 +112,7 @@ class Main {
 			token = null;
 			return ans;
 		}
-		
+
 		public String getLine() {
 			try {
 				return r.readLine();
@@ -118,92 +129,124 @@ class Main {
 			}
 			return sb.toString();
 		}
-		
+
+		public String[] getAllArr() {
+			return getAll().split("\n");
+		}
+
 		public String[] getLineArr() {
 			return getLine().split(" ");
 		}
+
+		public String[] getLineArr(String delimiter) {
+			return getLine().split(delimiter);
+		}
+
+		public int[] getIntArr() {
+			return Arrays.stream(getLineArr()).mapToInt(Integer::parseInt).toArray();
+		}
+
+		public int[] getIntArr(String delimiter) {
+			return Arrays.stream(getLineArr(delimiter)).mapToInt(Integer::parseInt).toArray();
+		}
+
+		public long[] getLongArr() {
+			return Arrays.stream(getLineArr()).mapToLong(Long::parseLong).toArray();
+		}
+
+		public long[] getLongArr(String delimiter) {
+			return Arrays.stream(getLineArr(delimiter)).mapToLong(Long::parseLong).toArray();
+		}
+
+		public double[] getDoubleArr() {
+			return Arrays.stream(getLineArr()).mapToDouble(Double::parseDouble).toArray();
+		}
+
+		public double[] getDoubleArr(String delimiter) {
+			return Arrays.stream(getLineArr(delimiter)).mapToDouble(Double::parseDouble).toArray();
+		}
+	}
+	
+	static long timeIt(Runnable r) {
+		long start = System.currentTimeMillis();
+		r.run();
+		long end = System.currentTimeMillis();
+		long time = end - start;
+		System.out.println("Time: " + time + " ms");
+		return time;
 	}
 
 	static class ArrayHelper {
 		public static void printArr(int[] array) {
-	        System.out.print("{");
-	        for (int i = 0; i < array.length; i++) {
-	            System.out.print(array[i]);
-	            if (i != array.length - 1) {
-	                System.out.print(", ");
-	            }
-	        }
-	        System.out.print("}\n");
-    	}
+			System.out.println(Arrays.toString(array));
+		}
+
+		public static void printArr(long[] array) {
+			System.out.println(Arrays.toString(array));
+		}
 
 		public static void printArr(String[] array) {
-	        System.out.print("{");
-	        for (int i = 0; i < array.length; i++) {
-	            System.out.print(array[i]);
-	            if (i != array.length - 1) {
-	                System.out.print(", ");
-	            }
-	        }
-	        System.out.print("}\n");
-    	}
+			System.out.println(Arrays.toString(array));
+		}
 
 		public static void printArr(char[] array) {
-	        System.out.print("{");
-	        for (int i = 0; i < array.length; i++) {
-	            System.out.print(array[i]);
-	            if (i != array.length - 1) {
-	                System.out.print(", ");
-	            }
-	        }
-	        System.out.print("}\n");
-    	}
+			System.out.println(Arrays.toString(array));
+		}
 
 		public static int indexOf(int[] array, int element) {
-	        for (int i = 0; i < array.length; i++) {
-	            if (array[i] == element)
-	                return i;
-	        }
-	        return -1;
-    	}
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == element)
+					return i;
+			}
+			return -1;
+		}
+
+		public static int indexOf(long[] array, long element) {
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == element)
+					return i;
+			}
+			return -1;
+		}
 
 		public static int indexOf(String[] array, String element) {
-	        for (int i = 0; i < array.length; i++) {
-	            if (array[i].equals(element))
-	                return i;
-	        }
-	        return -1;
-    	}
+			for (int i = 0; i < array.length; i++) {
+				if (array[i].equals(element))
+					return i;
+			}
+			return -1;
+		}
 
 		public static int indexOf(char[] array, char element) {
-	        for (int i = 0; i < array.length; i++) {
-	            if (array[i] == element)
-	                return i;
-	        }
-	        return -1;
-    	}
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == element)
+					return i;
+			}
+			return -1;
+		}
 
 		public static void swap(int[] array, int index1, int index2) {
-	        int temp = array[index1];
-	        array[index1] = array[index2];
-	        array[index2] = temp;
-    	}
+			int temp = array[index1];
+			array[index1] = array[index2];
+			array[index2] = temp;
+		}
 
 		public static void swap(String[] array, int index1, int index2) {
-	        String temp = array[index1];
-	        array[index1] = array[index2];
-	        array[index2] = temp;
-    	}
+			String temp = array[index1];
+			array[index1] = array[index2];
+			array[index2] = temp;
+		}
 
 		public static void swap(char[] array, int index1, int index2) {
-	        char temp = array[index1];
-	        array[index1] = array[index2];
-	        array[index2] = temp;
-    	}
+			char temp = array[index1];
+			array[index1] = array[index2];
+			array[index2] = temp;
+		}
 
 		public static int min(int[] arr) {
 			int min = Integer.MAX_VALUE;
-			for(int i : arr) {
-				if(i < min)
+			for (int i : arr) {
+				if (i < min)
 					min = i;
 			}
 			return min;
@@ -211,39 +254,35 @@ class Main {
 
 		public static int max(int[] arr) {
 			int max = Integer.MIN_VALUE;
-			for(int i : arr) {
-				if(i > max)
+			for (int i : arr) {
+				if (i > max)
 					max = i;
 			}
 			return max;
 		}
 
 		public static boolean contains(int[] arr, int value) {
-			for(int i : arr) {
-				if(i == value)
+			for (int i : arr) {
+				if (i == value)
 					return true;
 			}
 			return false;
 		}
 
 		public static boolean contains(String[] arr, String value) {
-			for(String i : arr) {
-				if(i.equals(value))
+			for (String i : arr) {
+				if (i.equals(value))
 					return true;
 			}
 			return false;
 		}
 
 		public static boolean contains(char[] arr, char value) {
-			for(char i : arr) {
-				if(i == value)
+			for (char i : arr) {
+				if (i == value)
 					return true;
 			}
 			return false;
-		}
-
-		public static float clamp(float val, float min, float max) {
-    		return Math.max(min, Math.min(max, val));
 		}
 
 		public static void reverse(int[] data) {
@@ -253,9 +292,17 @@ class Main {
 			}
 		}
 
-		public static int randomInt(int minInclusive, int maxExclusive) {
-			return (int)Math.floor(Math.random()*(maxExclusive-minInclusive+1)+minInclusive);
+		public static boolean inBounds(int coord1, int coord2, int size1, int size2) {
+			return coord1 >= 0 && coord1 < size1 && coord2 >= 0 && coord2 < size2;
 		}
+	}
+	
+	public static float clamp(float val, float min, float max) {
+		return Math.max(min, Math.min(max, val));
+	}
+
+	public static int randomInt(int minInclusive, int maxExclusive) {
+		return (int) (Math.random() * (maxExclusive - minInclusive) + minInclusive);
 	}
 
 	public static class PermIterator implements Iterator<int[]> {
@@ -383,6 +430,30 @@ class Main {
 
 		public void reset() {
 			count = 0;
+		}
+	}
+
+	// Wrapper class for primitive arrays to allow for hashmap storage
+	static class ArrayWrapper<T> {
+		private T[] array;
+
+		public ArrayWrapper(T[] array) {
+			this.array = array;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o)
+				return true;
+			if (o == null || getClass() != o.getClass())
+				return false;
+			ArrayWrapper<?> that = (ArrayWrapper<?>) o;
+			return Arrays.equals(array, that.array);
+		}
+
+		@Override
+		public int hashCode() {
+			return Arrays.hashCode(array);
 		}
 	}
 }
